a8864f7bae255be4f869e9b1ff7d16c510cd5eb8
Nidhee3 <nidhiinvit@gmail.com>
2026-03-16 12:27:02 +0530

    Initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div class="jumbotron text-center">
+      <h1 class="display-4">Hello, welcome to my eshop!</h1>
+      <p class="lead">
+        This is your one stop shop for all your shopping needs
+      </p>
+      <hr class="my-4" />
+      <p>
+        view all products here !
+      </p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">
+       Start Shopping and enjoy
+      </a>
+    </div>
+  );
+};
+export default Header;
---
+++ src/pages/AboutPage/index.jsx
@@ -0,0 +1,24 @@
+import Navbar from "../../components/Navbar"
+
+const AboutPage = () => {
+    return(
+        <>
+            <Navbar />
+            <div className="jumbotron text-center">
+                <h1 className="display-4">About Us</h1>
+                <p className="lead">This is about us page</p>
+            </div>
+            <div className="container">
+                <section>
+                    <p>
+                        This is Nidharshana's eshop website.
+                        </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repellendus facere maiores? Nulla eum explicabo repellat dolorum reiciendis ullam dignissimos, eveniet pariatur provident enim autem eaque nostrum itaque quae, maxime tempore? Animi accusantium culpa nihil eius cupiditate eligendi incidunt repudiandae? Nostrum blanditiis perspiciatis magni molestias inventore quaerat. Error voluptatem quis vitae eum expedita fuga assumenda tenetur similique, neque cupiditate sequi minima vero iste provident harum ullam tempora ab impedit modi delectus aut? Sint, nesciunt? Est vitae nulla perferendis iure maiores reprehenderit. Laudantium harum atque, temporibus nulla maiores optio at nemo ab suscipit quos asperiores neque quod? Blanditiis, delectus deleniti. Quis.
+                    </p>
+                </section>
+            </div>
+        </>
+    )   
+}
+export default AboutPage
